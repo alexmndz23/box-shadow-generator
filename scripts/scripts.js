cdacda1c9383876e3a591beb7019db4a5ac455a7
Iran Mendez <mgia.130195@gmail.com>
2023-11-11 18:53:16 -0800

    Ya funciona el cambio de color en la vista previa

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,34 @@
 window.addEventListener('load', () => {
+
   let changeTheme = () => {
     let body = document.querySelector('body')
     body.classList.toggle('dark')
   }
+
+  let changeCollapse = (shadow) => {
+    shadow.classList.toggle('show')
+  }
+  
+  let changeBackgroundColor = (element, color) => {
+    element.style.backgroundColor = color
+  }
+                                                                                                 
+  let setRangeValueFromInput = (input) => {
+    let shadowProperty = input.closest('#shadowProperty')
+    let range = shadowProperty.querySelector('range')
+    range.value = input.value
+  }
+
   let changeThemeButton = document.querySelector('#changeTheme')
   changeThemeButton.addEventListener('click', () => changeTheme())
+
+  let shadows = document.querySelectorAll('.shadow')
+  shadows.forEach(shadow => {
+    let shadowTitle = shadow.querySelector('.shadowTitle')
+    shadowTitle.addEventListener('click', () => changeCollapse(shadow))
+  })
+
+  let inputChangeBgColor = document.querySelector('#backgroundColorShadowPreview')
+  let shadowPreview = document.querySelector('#shadowPreview')
+  inputChangeBgColor.addEventListener('input', (e) => changeBackgroundColor(shadowPreview, e.target.value))
 })
