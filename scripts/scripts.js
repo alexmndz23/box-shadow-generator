7d08234ec335e06f8e3427d760197f7c5f0a8794
Iran Mendez <mgia.130195@gmail.com>
2023-11-20 16:53:11 -0800

    Cambio en el cambio de color de la vista previa

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,7 +28,24 @@ window.addEventListener('load', () => {
     shadowTitle.addEventListener('click', () => changeCollapse(shadow))
   })
 
+  let copyCode = () => {
+    let codePreview = document.querySelector('#codePreview')
+    codePreview.select()
+    document.execCommand("copy");
+  }
+  let copyCodeBtn = document.querySelector('#copyCode')
+  copyCodeBtn.addEventListener('click', copyCode)
+
   let inputChangeBgColor = document.querySelector('#backgroundColorShadowPreview')
   let shadowPreview = document.querySelector('#shadowPreview')
   inputChangeBgColor.addEventListener('input', (e) => changeBackgroundColor(shadowPreview, e.target.value))
+
+  shadowPreview.addEventListener('click', (event) => {
+    const { clientX, clientY } = event
+    const { top, left } = shadowPreview.getBoundingClientRect()
+    inputChangeBgColor.style.top = `${clientY - top}px`
+    inputChangeBgColor.style.left = `${clientX - left}px`
+    
+    inputChangeBgColor.click()
+  })
 })
